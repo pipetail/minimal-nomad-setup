40960eef31ba20dc629da9b8c54b1498f14231e4
Stepan Vrany <stepan@vrany.dev>
2020-05-01 10:13:48 +0000

    fix router names

--- terraform/loadbalancer/dns.tf
+++ terraform/loadbalancer/dns.tf
@@ -4,3 +4,10 @@ resource "digitalocean_record" "nomad_traefik" {
   name   = "traefik"
   value  = digitalocean_loadbalancer.public.ip
 }
+
+resource "digitalocean_record" "nomad_nginx" {
+  domain = "stepanvrany.cz"
+  type   = "A"
+  name   = "nginx"
+  value  = digitalocean_loadbalancer.public.ip
+}
